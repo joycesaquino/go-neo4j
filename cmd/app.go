dfc0c286e626add698709bd45149a7f313cf4441
joyce.aquino <joyce.aquino@b2wdigital.com>
2020-05-25 11:36:10 -0300

    [new] Removendo os exemplos do handler

--- cmd/app.go
+++ cmd/app.go
@@ -3,40 +3,10 @@ package main
 import (
 	"dera-services-api/internal/service"
 	"github.com/gorilla/mux"
-	"github.com/neo4j/neo4j-go-driver/neo4j"
 	"net/http"
 )
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/service", service.NewController().Insert).Methods(http.MethodPost)
-	router.HandleFunc("/service_delete", service.NewController().Create).Methods(http.MethodDelete)
-
-	//API Interface de Programação de Aplicações
-	// Camada HTTP
-
-	//	CRUD
-	//Create -> POST
-	//Read -> GET
-	//Update -> PUT
-	//Delete -> DELETE
-
-	//SERVICES
-
-}
-
-type Operation struct {
-	Value    float64
-	Code     string
-	Type     string
-	DateBuy  neo4j.Date
-	DateSale neo4j.Date
-	Taxes    float64
-	Profit   float64
-}
-
-func Calculate(operation Operation) {
-	if operation.DateSale.Day() > operation.DateBuy.Day() {
-		operation.Taxes = operation.Value * 2
-	}
 }
